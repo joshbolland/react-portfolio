f0cb7f2f22b63268156512262d3a4bdf384a196b
joshbolland <jjm.bolland@gmail.com>
2025-02-26 12:12:07 +0000

    after react-router-dom redesign, was able to extract navbar out to its own component file - updated imports accordingly

--- src/App.js
+++ src/App.js
@@ -4,36 +4,17 @@ import "./card.css";
 import "./footer.css";
 import "./home.css";
 import "./navbar.css";
+import Navbar from "./Navbar";
 import About from "./About";
 import Project from "./Project";
 import Footer from "./Footer";
 import Contact from "./Contact";
-import Me from "./me.jpeg";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="navbar-wagon">
-        <div className="navbar-wagon-right hidden-xs hidden-sm">
-          <Link to="/" className="navbar-wagon-item navbar-wagon-link">
-            About
-          </Link>
-          <Link to="/projects" className="navbar-wagon-item navbar-wagon-link">
-            Projects
-          </Link>
-          <Link to="/contact" className="navbar-wagon-item navbar-wagon-link">
-            Contact
-          </Link>
-        </div>
-        <Link to="/">
-          <img
-            src={Me}
-            alt="Headshot of me, Joshua Bollad"
-            className="avatar"
-          ></img>
-        </Link>
-      </div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<About />} />
         <Route path="/projects" element={<Project />} />
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
+import Me from "./me.jpeg";
+
 export default function Navbar() {
-  return;
+  return (
+    <div className="navbar-wagon">
+      <div className="navbar-wagon-right hidden-xs hidden-sm">
+        <Link to="/" className="navbar-wagon-item navbar-wagon-link">
+          About
+        </Link>
+        <Link to="/projects" className="navbar-wagon-item navbar-wagon-link">
+          Projects
+        </Link>
+        <Link to="/contact" className="navbar-wagon-item navbar-wagon-link">
+          Contact
+        </Link>
+      </div>
+      <Link to="/">
+        <img
+          src={Me}
+          alt="Headshot of me, Joshua Bollad"
+          className="avatar"
+        ></img>
+      </Link>
+    </div>
+  );
 }
