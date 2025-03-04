cb4aa906f7a4cea27e98c951c73f9370dcad0276
joshbolland <jjm.bolland@gmail.com>
2025-03-04 14:04:41 +0000

    added use location function to navbar to trigger re render on navbar link navigation to fully unmount component every time - should be fine with size of components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Me from "./me.jpeg";
 
 export default function Navbar() {
+  const location = useLocation();
+
   return (
     <div className="navbar-wagon">
       <div className="navbar-wagon-right hidden-xs hidden-sm">
-        <Link to="/" className="navbar-wagon-item navbar-wagon-link">
+        <Link
+          to="/"
+          key={location.pathname}
+          className="navbar-wagon-item navbar-wagon-link"
+        >
           About
         </Link>
-        <Link to="/projects" className="navbar-wagon-item navbar-wagon-link">
+        <Link
+          to="/projects"
+          key={location.pathname}
+          className="navbar-wagon-item navbar-wagon-link"
+        >
           Projects
         </Link>
-        <Link to="/contact" className="navbar-wagon-item navbar-wagon-link">
+        <Link
+          to="/contact"
+          key={location.pathname}
+          className="navbar-wagon-item navbar-wagon-link"
+        >
           Contact
         </Link>
       </div>
-      <Link to="/">
+      <Link to="/" key={location.pathname}>
         <img
           src={Me}
           alt="Headshot of me, Joshua Bollad"
