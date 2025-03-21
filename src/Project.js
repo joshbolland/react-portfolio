c1a01f6723b1dcbe8fe507165da05505602d3215
joshbolland <jjm.bolland@gmail.com>
2025-03-21 07:52:06 +0000

    updated projects section to reorder and replace musessions with taildo. jammer moved to last and portfolio to first

--- src/Project.js
+++ src/Project.js
@@ -2,6 +2,24 @@ import NowPlaying from "./NowPlaying";
 
 const projectData = [
   {
+    url: "https://joshbolland.netlify.app/",
+    bgImageUrl:
+      "https://res.cloudinary.com/dohcbj8ua/image/upload/v1741774717/me_dtq5tt.jpg",
+    altDesc: "Headshot of Josh Bolland",
+    name: "Portfolio",
+    tagline: "This portfolio site!",
+    ghURL: "https://github.com/joshbolland/react-portfolio",
+  },
+  {
+    url: "https://taildo.netlify.app/",
+    bgImageUrl:
+      "https://res.cloudinary.com/dohcbj8ua/image/upload/v1742543357/tick_kdyf2h.png",
+    altDesc: "Blue tick logo",
+    name: "TailDo",
+    tagline: "Manage your to-dos",
+    ghURL: "https://github.com/joshbolland/react-todo",
+  },
+  {
     url: "https://jammerapp.herokuapp.com/",
     bgImageUrl:
       "https://res.cloudinary.com/wagon/image/upload/v1542891739/vuab5xxyqd4qzcsqvjdp.jpg",
@@ -10,24 +28,6 @@ const projectData = [
     name: "Jammer",
     tagline: "Connecting Musicians",
     ghURL: "https://github.com/joshbolland/jammer",
-  },
-  {
-    url: "https://musessions.herokuapp.com",
-    bgImageUrl:
-      "https://res.cloudinary.com/dohcbj8ua/image/upload/v1544350590/background2.jpg",
-    altDesc: "Drummer plays wet drumkit outside of a garage",
-    name: "Musessions",
-    tagline: "Find and Hire Session Musicians",
-    ghURL: "https://github.com/joshbolland/musessions",
-  },
-  {
-    url: "https://joshbolland.netlify.app/",
-    bgImageUrl:
-      "https://res.cloudinary.com/dohcbj8ua/image/upload/v1741774717/me_dtq5tt.jpg",
-    altDesc: "Headshot of Josh Bolland",
-    name: "Portfolio",
-    tagline: "This portfolio site!",
-    ghURL: "https://github.com/joshbolland/react-portfolio",
   },
 ];
 
